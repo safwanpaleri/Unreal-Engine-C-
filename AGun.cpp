9fc4e2c5b490d1df4ebf4b4c5a9c06172326493f
safwanpaleri <safwanpaleri@gmail.com>
2021-11-13 13:24:24 +0530

    Update AGun.cpp

--- AGun.cpp
+++ AGun.cpp
@@ -1,5 +1,4 @@
-
-
+//Header files.
 #include "AGun.h"
 #include "Components/SkeletalMeshComponent.h"
 #include "Kismet/GameplayStatics.h"
@@ -8,8 +7,9 @@
 // Sets default values
 AAGun::AAGun()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+ 	// Set this actor to call Tick() every frame.
 	PrimaryActorTick.bCanEverTick = true;
+	//Attaching Gun object to this Script.
 	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root Component"));
 	SetRootComponent(Root);
 	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Gun"));
@@ -20,45 +20,49 @@ AAGun::AAGun()
 void AAGun::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
 void AAGun::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
+//Function resposible for shooting.
 void AAGun::TriggerGun()
 {
-	//UE_LOG(Log,Warning,TEXT("Shoot"));
+	//Spawn vfx.
 	UGameplayStatics::SpawnEmitterAttached(MuzzleFlash,Mesh,TEXT("MuzzleFlashSocket"));
-	// viewpoint
+	
+	//getting viewpoint
 	APawn* OwnerPawn = Cast<APawn>(GetOwner());
 	if(!OwnerPawn) return;
 	AController* OwnerController = OwnerPawn->GetController();
 	if(!OwnerController) return;
-
 	FVector Location; 
 	FRotator Rotation;
 	OwnerController->GetPlayerViewPoint(Location,Rotation);
-	//Raycast
+	
+	//Raycasting
 	FHitResult Hit;
 	FVector End = Location + Rotation.Vector() * MaxRange;
 	
 	FCollisionQueryParams TraceParams(FName(TEXT("")),false,GetOwner());
 	bool bSucess=GetWorld()->LineTraceSingleByChannel(Hit,Location,End,ECollisionChannel::ECC_GameTraceChannel1,TraceParams);
+	
+	//if raycasting hit an object
 	if(bSucess)
-	{
+	{	
+		//shoot in the direction,
+		//spawn vfx at the hit location.
 		FVector ShotDirection = -(Rotation.Vector());
 		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(),ImpactFlash,Hit.Location,ShotDirection.Rotation());
 		FPointDamageEvent DealDamage(DamageValue, Hit, ShotDirection, nullptr);
+		//if hit any Actor, decrease damage of that actor.
 		if(Hit.GetActor() != nullptr)
 		{
 			Hit.GetActor()->TakeDamage(DamageValue, DealDamage, OwnerController, this);
 		}
 	}
-
 }
 
